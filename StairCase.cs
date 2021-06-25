a4762ac124eb87dcdce88e0542b722ef41a957e1
takasidk <57638212+takasidk@users.noreply.github.com>
2021-06-25 18:44:00 +0530

    day2-problem3

---
+++ StairCase.cs
@@ -0,0 +1,43 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'staircase' function below.
+     *
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static void staircase(int n)
+    {
+        for(int i=1;i<n+1;++i){
+            Console.WriteLine(new string(' ',n-i)+new string('#',i));
+        }
+        
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        Result.staircase(n);
+    }
+}
+
