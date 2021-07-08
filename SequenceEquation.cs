953e783c52d442e8730058173c433b0a6b941251
unknown <dineshkoushik769@gmail.com>
2021-07-08 17:40:34 +0530

    DAy8-problem2

---
+++ SequenceEquation.cs
@@ -0,0 +1,53 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Link:https://www.hackerrank.com/challenges/permutation-equation/problem
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY p as parameter.
+     */
+
+    public static List<int> permutationEquation(List<int> p)
+    {
+        var resList=new List<int>();
+        for(int i=1;i<=p.Count;++i){
+            resList.Add(p.IndexOf(p.IndexOf(i)+1)+1);
+        }
+        return resList;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> p = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(pTemp => Convert.ToInt32(pTemp)).ToList();
+
+        List<int> result = Result.permutationEquation(p);
+
+        textWriter.WriteLine(String.Join("\n", result));
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
